50165f7fb29e4e641ce6bf2f4c796ec2ce2838ef
caganniflehim <omercagand@gmail.com>
2023-11-11 16:42:21 +0300

    Initial commit

---
+++ Inceleme/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inceleme
+{
+    class Program
+    {
+        static string s2 = "nur";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("naber:");
+
+            Console.WriteLine("isim ne:");
+            string ısım = Console.ReadLine();
+            Console.WriteLine("soy ismin ne:");
+            string soyısım = Console.ReadLine();
+
+            Console.WriteLine(" yaşadınığı yer:");
+            string yasadıgınYer = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("isim "  + ısım );
+            Console.WriteLine("soyısım "+ soyısım);
+            Console.WriteLine("yaşadığı yer " + yasadıgınYer);
+            Console.ReadLine();
+
+
+
+           
+        }
+    }
+}
